b6ed58410f907cabb6ad45706f5d37853b44eaec
Jeet <jetdeula@gmail.com>
2024-05-24 19:25:04 +0545

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,14 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      <link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
-/>
-<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
-      </head>
+  
       <body className={inter.className}>{children}</body>
     </html>
   );
