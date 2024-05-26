76e027710ed5839a1fda05f20e1680b1d5de2030
Jeet <jetdeula@gmail.com>
2024-05-26 20:31:48 +0545

    updated

--- src/app/Components/Assist.jsx
+++ src/app/Components/Assist.jsx
@@ -4,13 +4,13 @@ import { assist } from '../db/mockdata'
 const Assist = () => {
   return (
     <main>
-        <div className='w-[1100px] mx-auto mt-20 text-center'>
+        <div className='max-w-[1100px] mx-auto mt-20 text-center'>
             <h2 className='font-semibold text-2xl border-b-4 w-fit mx-auto border-[#F4310A] py-2'>We Assist You With</h2>
-            <div className='grid grid-cols-3 gap-y-16 gap-x-6 my-14 relative'>
+            <div className='grid lg:grid-cols-3 grid-cols-1 justify-center gap-y-16 gap-x-6 my-14 mx-2 relative'>
               {assist.map((assist, index) => (
                 <div key={index} className='relative'>
                     <img src={assist.img} alt="img" className='bg-[#F4310A] w-16 h-16 absolute top-[-45px] z-10 left-8 rounded-full p-2 border-4 border-white'/>
-                   <div className='bg-gray-100 hover:bg-[#F4310A] hover:text-white duration-500 ease-in-out cursor-pointer rounded-3xl py-10 px-4 h-80 grid gap-2 text-left'>
+                   <div className='bg-gray-100 lg:w-80 max-w-[50em] hover:bg-[#F4310A] hover:text-white duration-500 ease-in-out cursor-pointer rounded-3xl py-10 px-4 lg:h-80 grid gap-2 text-left'>
                    <h2 className='font-semibold text-xl'>{assist.title}</h2>
                     <p className='text-sm leading-7'>{assist.desc}</p>
                     <a href="#" className='font-semibold'>{assist.link}</a>
--- src/app/Components/HeroSection.jsx
+++ src/app/Components/HeroSection.jsx
@@ -7,9 +7,9 @@ const HeroSection = () => {
   return (
     <main>
       <Slider />
-      <div className="mt-16 text-center">
-        <h2 className="font-semibold text-3xl text-purple-900">PROFESSIONAL VISA AND EDUCATION SERVICES</h2>
-        <p className="px-40 py-5  mx-auto text-gray-500">
+      <div className="md:mt-16 mt-4 text-center">
+        <h2 className="font-semibold md:text-3xl text-2xl text-purple-900">PROFESSIONAL VISA AND EDUCATION SERVICES</h2>
+        <p className="max-w-[1200px] py-5  md:mx-auto mx-2 text-gray-500">
           Professional Visa and Education Services Pvt. Ltd. (Pro-Visa) is a
           professional visa and educational consultancy authorized by the
           Ministry of Education (MOE) and offering free education to students
