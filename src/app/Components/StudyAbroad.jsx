e78f5921cca1b20676101bed08ed73713687646d
Jeet <jetdeula@gmail.com>
2024-06-01 10:32:00 +0545

    updated

--- src/app/Components/StudyAbroad.jsx
+++ src/app/Components/StudyAbroad.jsx
@@ -39,13 +39,13 @@ const BigImgAnimation={
           initial = "initial"
           whileInView="animate"
           transition={{duration:0.5,ease:"easeInOut"}}
-          viewport={{once:true,amount:1}}
+          viewport={{once:true,amount:0.5}}
           className='grid grid-cols-2 gap-y-10 gap-4'>
             <div className='relative overflow-hidden group'>
             <img src="https://provisa.com.np/wp-content/uploads/2022/04/japan.jpg.webp" alt="" className='w-[100%] h-60 object-cover group-hover:scale-125 ease-in-out duration-500' />
-           <div className=' text-white md:text-3xl text-xl absolute inset-0 text-left px-4 bg-gradient-to-b from-transparent via-transparent to-black md:translate-y-[62%] translate-y-[55%] group-hover:translate-y-0 py-10  group-hover:bg-orange-400  group-hover:bg-opacity-75  transition-ease-in-out duration-500 ' >
+           <div className=' text-white md:text-3xl text-xl absolute inset-0 text-left  px-4 bg-gradient-to-b from-transparent via-transparent to-black md:translate-y-[62%] translate-y-[55%] group-hover:translate-y-0 py-10  group-hover:bg-orange-400  group-hover:bg-opacity-75  transition-ease-in-out duration-500 ' >
             <h2 className='font-bold text-[18px] border-b pb-1'>STUDY IN JAPAN</h2>
-            <p className='text-[14px] font-bold'>Education System in Japan In the decades following World [...]</p>
+            <p className='text-[14px] font-bold leading-6'>Education System in Japan In the decades following World [...]</p>
             <button className='bg-[#0C71B7] font-semibold text-sm w-36 py-4'>Learn More</button>
            </div>
             </div>
@@ -53,7 +53,7 @@ const BigImgAnimation={
             <img src="https://provisa.com.np/wp-content/uploads/2022/04/canal-g1228989ee_640.jpg.webp" alt="" className='w-[100%] h-60 object-cover group-hover:scale-125 ease-in-out duration-500' />
             <div className=' text-white md:text-3xl text-xl absolute inset-0 text-left px-4 bg-gradient-to-b from-transparent via-transparent to-black md:translate-y-[62%] translate-y-[55%]  group-hover:translate-y-0 py-10 group-hover:bg-orange-400 group-hover:bg-opacity-75  transition-ease-in-out duration-500 ' >
             <h2 className='font-bold text-[18px] border-b pb-1'>STUDY IN EUROPE</h2>
-            <p className='text-[14px] font-bold'>In terms of Research and education Europe is[....]</p>
+            <p className='text-[14px] font-bold leading-6'>In terms of Research and education Europe is[....]</p>
             <button className='bg-[#0C71B7] font-semibold text-sm w-36 py-4'>Learn More</button>
            </div>
             </div>
@@ -61,7 +61,7 @@ const BigImgAnimation={
             <img src="https://provisa.com.np/wp-content/uploads/2020/09/jason-hafso-C2keINMOhIE-unsplash.jpg.webp" alt="" className='w-[100%] h-60 object-cover group-hover:scale-125 ease-in-out duration-500' />
             <div className=' text-white md:text-3xl text-xl absolute inset-0 text-left px-4 bg-gradient-to-b from-transparent via-transparent to-black md:translate-y-[62%] translate-y-[55%]  group-hover:translate-y-0 py-10 group-hover:bg-orange-400 group-hover:bg-opacity-75  transition-ease-in-out duration-500 ' >
             <h2 className='font-bold text-[18px] border-b pb-1'>STUDY IN CANADA</h2>
-            <p className='text-[14px] font-bold'>Education System In Canada It's common knowledge that studying [...]</p>
+            <p className='text-[14px] font-bold leading-6'>Education System In Canada It's common knowledge that studying [...]</p>
             <button className='bg-[#0C71B7] font-semibold text-sm w-36 py-4'>Learn More</button>
            </div>
             </div>
@@ -69,7 +69,7 @@ const BigImgAnimation={
             <img src="https://provisa.com.np/wp-content/uploads/2021/02/remi-thorel-304R7qkgHe8-unsplash.jpg.webp" alt="" className='w-[100%] h-60 object-cover group-hover:scale-125 ease-in-out duration-500' />
             <div className=' text-white md:text-3xl text-xl absolute inset-0 text-left px-4 bg-gradient-to-b from-transparent via-transparent to-black md:translate-y-[62%] translate-y-[55%]  group-hover:translate-y-0 py-10 group-hover:bg-orange-400 group-hover:bg-opacity-75  transition-ease-in-out duration-500 ' >
             <h2 className='font-bold text-[18px] border-b pb-1'>STUDY IN USA</h2>
-            <p className='text-[14px] font-bold'>Education System in USA There are two major intakes [...]</p>
+            <p className='text-[14px] font-bold leading-6'>Education System in USA There are two major intakes [...]</p>
             <button className='bg-[#0C71B7] font-semibold text-sm w-36 py-4'>Learn More</button>
            </div>
             </div>
