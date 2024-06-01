73a1da75426bd10328814f2a8620c3b87f5b859a
Jeet <jetdeula@gmail.com>
2024-06-01 07:59:42 +0545

    updated

--- src/app/Components/Contact.jsx
+++ src/app/Components/Contact.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
+"use client"
+import React, { useRef } from 'react'
+import { motion } from "framer-motion"
 
 const Contact = () => {
+  const transitiony = {
+    initial:{y:-15, opacity: 0},
+    animate:{  y: 0,opacity:1 } 
+  }
+  const transition = {
+    initial:{y:15,opacity:0},
+    animate:{y:0,opacity:1}
+  }
   return (
    <main>
     <div className='max-w-[1100px] md:mx-auto mx-2 flex xl:flex-nowrap flex-wrap justify-center gap-6 my-12 relative'>
-        <figure className='w-[40em]'>
+    <motion.div
+    variants={transitiony}
+       initial="initial"
+       whileInView="animate"
+       viewport={{ once: true,amount:1 }}
+       transition={{ ease: "easeOut", duration: 1 }}
+        className='w-[40em]'>
             <img src="https://provisa.com.np/wp-content/uploads/2022/10/confused-provisa-nepal.jpg.webp" alt="" />
             <div className='md:absolute hidden top-80 mx-40 bg-orange-400 opacity-75 h-60 p-6'>
               <p className='text-2xl font-bold text-white'>Choosing Us As Your Education Partner, We Can Assure You Your Future While You Focus On Your Study.</p>
             </div>
-        </figure>
+        </motion.div>
 
-        <div className='bg-gray-100 max-w-[40em]  py-4 px-8'>
+        <motion.div 
+        variants={transition}
+        initial="initial"
+        whileInView="animate"
+        transition={{duration:1,ease:"easeInOut"}}
+        viewport={{once:true}}
+        className='bg-gray-200 max-w-[40em]  py-4 px-8'>
             <form action="#">
             <h2 className='font-semibold text-2xl'>For Your Inquiries</h2>
                <div className='grid mt-4 gap-2'>
@@ -34,7 +56,7 @@ const Contact = () => {
                <button className='bg-[#F4310A] w-28 py-2 rounded-lg text-white mt-2'>Submit</button>
                </div>
             </form>
-        </div>
+        </motion.div>
     </div>
    </main>
   )
